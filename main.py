54e9bd488d281c7229c03fb3da56cf2829512a13
wloosma2 <william.loosman@enexis.nl>
2022-08-03 16:46:13 +0200

    basic gantt with styling and counting

--- main.py
+++ main.py
@@ -1,26 +1,129 @@
-import datetime
-from math import floor
 from pathlib import Path
-import calendar
 
-from openpyxl import Workbook
-from openpyxl.styles import Font, Border, Side, Alignment
+from openpyxl import Workbook, formatting
+from openpyxl.formatting.rule import CellIsRule, FormulaRule
+from openpyxl.styles import Font, Border, Side, Alignment, NamedStyle, Fill, PatternFill
+from openpyxl.styles.fills import FILL_PATTERN_DARKHORIZONTAL
 from openpyxl.utils import get_column_letter
 from openpyxl.worksheet.worksheet import Worksheet
 
-from time_util import days_left_in_year
+from time_iterate import TimeIteratorMode, TimeIterator
 
 name = "gantt"
 path = Path.home() / (name + '.xlsx')
 # path = 'custom/path'
 
+start_year = 2022
+start_month = 8
+months_duration = 13
+mode = TimeIteratorMode.DAYS_NO_WEEKEND
+# mode = TimeIteratorMode.DAYS
+
+timeIterate = TimeIterator(start_year, start_month, months_duration, mode)
+
+matrix_h_start = 3
+matrix_v_start = 7
+matrix_h_end = len(timeIterate)
+matrix_v_end = 200
+matrix_color = 'BFBFBF'
+
+red = 'FF0000'
+
 italic = Font(italic=True)
 bold = Font(bold=True)
 small = Font(size=9)
+matrix_fill = PatternFill(patternType='solid', bgColor=red)
 left_align = Alignment(horizontal='left')
 center_align = Alignment(horizontal='center', vertical='center')
-
 border = Border(bottom=Side(style='thin'))
+matrix_border = Border(bottom=Side(style='thin', color=matrix_color), top=Side(style='thin', color=matrix_color), left=Side(style='thin', color=matrix_color), right=Side(style='thin', color=matrix_color))
+
+styles = {}
+
+YEAR_EVEN = 'year_even'
+year_even = NamedStyle(name=YEAR_EVEN)
+year_even.font = Font(color='00FFFFFF')
+year_even.fill = PatternFill(patternType='solid', fgColor='808080')
+year_even.alignment = left_align
+styles[YEAR_EVEN] = year_even
+
+YEAR_ODD = 'year_odd'
+year_odd = NamedStyle(name=YEAR_ODD)
+year_odd.font = Font(color='00FFFFFF')
+year_odd.fill = PatternFill(patternType='solid', fgColor='595959')
+year_odd.alignment = left_align
+styles[YEAR_ODD] = year_even
+
+month_colors = ('9BBB59', '4BACC6', 'F79646', 'E0D60E', '8064A2', 'C0504D', '9BBB59', '4BACC6', 'F79646', 'E0D60E', '8064A2', 'C0504D')
+month_day_colors = ('D8E4BC', 'B7DEE8', 'FCD5B4', 'F8F388', 'CCC0DA', 'E6B8B7', 'D8E4BC', 'B7DEE8', 'FCD5B4', 'F8F388', 'CCC0DA', 'E6B8B7')
+
+i = 0
+for c in month_colors:
+    i += 1
+    l = 'month_' + str(i)
+    month_s = NamedStyle(name=l)
+    month_s.font = Font(color='00FFFFFF', italic=True)
+    month_s.fill = PatternFill(fill_type='solid', fgColor=c)
+    month_s.alignment = left_align
+
+    styles[l] = month_s
+
+WEEK_EVEN = 'week_even'
+week_even = NamedStyle(name=WEEK_EVEN)
+week_even.font = Font(size=9)
+week_even.fill = PatternFill(patternType='solid', fgColor='F2F2F2')
+week_even.alignment = left_align
+styles[WEEK_EVEN] = year_even
+
+WEEK_ODD = 'week_odd'
+week_odd = NamedStyle(name=WEEK_ODD)
+week_odd.font = Font(size=9)
+week_odd.fill = PatternFill(patternType='solid', fgColor='D9D9D9')
+week_odd.alignment = left_align
+styles[WEEK_ODD] = year_even
+
+DAY_EVEN = 'day_even'
+day_even = NamedStyle(name=DAY_EVEN)
+day_even.font = Font(size=9, italic=True)
+day_even.fill = PatternFill(patternType='solid', fgColor='F2F2F2')
+day_even.alignment = center_align
+styles[DAY_EVEN] = year_even
+
+DAY_ODD = 'day_odd'
+day_odd = NamedStyle(name=DAY_ODD)
+day_odd.font = Font(size=9, italic=True)
+day_odd.fill = PatternFill(patternType='solid', fgColor='D9D9D9')
+day_odd.alignment = center_align
+styles[DAY_ODD] = year_even
+
+matrix_font = Font(size=16, color=red)
+
+MATRIX_CELL_EVEN = 'matrix_cell_even'
+matrix_cell_even = NamedStyle(name=MATRIX_CELL_EVEN)
+matrix_cell_even.font = matrix_font
+matrix_cell_even.border = matrix_border
+matrix_cell_even.fill = PatternFill(patternType='solid', fgColor='F2F2F2')
+matrix_cell_even.alignment = center_align
+styles[MATRIX_CELL_EVEN] = year_even
+
+MATRIX_CELL_ODD = 'matrix_cell_odd'
+matrix_cell_odd = NamedStyle(name=MATRIX_CELL_ODD)
+matrix_cell_odd.font = matrix_font
+matrix_cell_odd.border = matrix_border
+matrix_cell_odd.fill = PatternFill(patternType='solid', fgColor='D9D9D9')
+matrix_cell_odd.alignment = center_align
+styles[MATRIX_CELL_ODD] = year_even
+
+i = 0
+for c in month_day_colors:
+    i += 1
+    l = 'month_day_' + str(i)
+    month_s = NamedStyle(name=l)
+    month_s.font = Font(size=9)
+    month_s.fill = PatternFill(fill_type='solid', fgColor=c)
+    month_s.alignment = center_align
+
+    styles[l] = month_s
 
 wb = Workbook()
 worksheet: Worksheet = wb.active
@@ -30,86 +133,81 @@ worksheet.column_dimensions['A'].width = 1.2
 worksheet.column_dimensions['B'].width = 43
 worksheet.column_dimensions['C'].width = 9
 
-max_columns_in_excel = 16384
-for i in range(1, 16384):
+# Take into consideration that the max cols in Excel are 16384
+max_columns_in_excel = matrix_h_end + matrix_h_start + 1
+for i in range(1, max_columns_in_excel):
     if i >= 4:
         worksheet.column_dimensions[get_column_letter(i)].width = 2.8
-
-    worksheet.row_dimensions[i].height = 15
     worksheet.cell(row=6, column=i).border = border
+
+# Prepare 200 rows to use as task -> this should be more than enough
+worksheet.row_dimensions[1].height = 3
+for i in range(2, matrix_v_end + 1):
+    worksheet.row_dimensions[i].height = 15
 
 worksheet.cell(row=6, column=2).value = 'Actie'
 worksheet.cell(row=6, column=2).font = bold
+worksheet.cell(row=6, column=3).value = 'Duur'
+worksheet.cell(row=6, column=3).font = bold
 
-day_names = ('ma', 'di', 'wo', 'do', 'vr', 'za', 'zo')
-month_names = ('Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'Sebtember', 'Oktober', 'November', 'December')
-c = calendar.Calendar()
+for i in range(matrix_v_start, matrix_v_end + 1):
+    f = f'=COUNTIF(D{i}:ZZ{i},"*")'
+    worksheet.cell(row=i, column=3).value = f
 
-start_year = 2022
-start_month = 8
-months_duration = 12
+worksheet.conditional_formatting.add(f'$D${matrix_v_start}:$ZZ${matrix_v_end}', FormulaRule(formula=[f'D{matrix_v_start}<>""'], stopIfTrue=False, fill=matrix_fill, font=matrix_font))
 
-end_year = start_year + floor((months_duration - start_month) / 12) + 1
-end_month = (months_duration - (12 - start_month)) % 12 - 1
+days_in_week = mode.value
+sum_days_in_year = 1
+sum_days_in_month = 1
+for passed_months, year, days_left_in_year, month, month_name, days_in_month, i, week_number, day_number, day, day_name in timeIterate:
+    column_i = matrix_h_start + i
 
-ym_start = 12 * start_year + start_month - 1
-ym_end = 12 * end_year + end_month
-i = 0
-ii = 0
-matrix_start = 3
-week_start_index = 4
-week_start_end_index = 10
-days_in_month = calendar.monthrange(start_year, start_month)[1]
-days_in_year = days_left_in_year(start_year, start_month)
-sum_year_thing = 1
-for ym in range(ym_start, ym_end):
-    y, m = divmod(ym, 12)
-    year = y
-    month = m + 1
-    i += 1
+    # Year
+    if i % sum_days_in_year == 0:
+        row = 2
+        worksheet.cell(row=row, column=column_i).value = year
+        worksheet.cell(row=row, column=column_i).style = year_even if year % 2 == 0 else year_odd
+        worksheet.merge_cells(start_row=row, start_column=column_i,
+                              end_row=row,   end_column=column_i + days_left_in_year - 1)
 
-    for day_number in c.itermonthdays(year, month):
-        if day_number == 0:
-            continue
+        sum_days_in_year += days_left_in_year
 
-        ii += 1
-        day = calendar.weekday(year, month, day_number)
-        week_number = datetime.date(year, month, day_number).isocalendar().week
+    # Month
+    if i % sum_days_in_month == 0:
+        row = 3
+        month_style = styles['month_' + str(month)]
+        worksheet.cell(row=row, column=column_i).value = month_name
+        worksheet.cell(row=row, column=column_i).style = month_style
+        worksheet.merge_cells(start_row=row, start_column=column_i,
+                              end_row=row,   end_column=column_i + days_in_month - 1)
 
-        if ii % sum_year_thing == 0:
-            worksheet.cell(row=2, column=ii + matrix_start).value = year
-            worksheet.cell(row=2, column=ii + matrix_start).alignment = left_align
-            worksheet.merge_cells(start_row=2, start_column=ii + matrix_start,
-                                  end_row=2, end_column=ii + matrix_start + days_in_year - 1)
+        sum_days_in_month += days_in_month
 
-            next_year = year + 1
-            sum_year_thing += days_in_year
-            days_in_year = days_left_in_year(next_year, end_month=end_month) if end_year == next_year else days_left_in_year(next_year)
+    week_is_even = week_number % 2 == 0
 
-        if day_number == 1:
-            worksheet.cell(row=3, column=ii + matrix_start).value = month_names[month - 1]
-            worksheet.cell(row=3, column=ii + matrix_start).alignment = left_align
-            worksheet.merge_cells(start_row=3, start_column=ii + matrix_start,
-                                  end_row=3, end_column=ii + matrix_start + days_in_month - 1)
+    # Week number
+    if i % days_in_week == 1:
+        row = 4
+        end_column_i = column_i + days_in_week - 1
 
-            next_month = calendar._nextmonth(year=year, month=month)[1]
-            days_in_month = calendar.monthrange(year, next_month)[1] if next_month > 1 else calendar.monthrange(year + 1, next_month)[1]
+        worksheet.cell(row=row, column=column_i).value = week_number
+        worksheet.cell(row=row, column=column_i).style = week_even if week_is_even else week_odd
+        worksheet.merge_cells(start_row=row, start_column=column_i, end_row=row, end_column=end_column_i)
 
-        if ii % 7 == 1:
-            worksheet.cell(row=4, column=week_start_index).value = week_number
-            worksheet.cell(row=4, column=week_start_index).alignment = left_align
-            worksheet.merge_cells(start_row=4, start_column=week_start_index, end_row=4, end_column=week_start_end_index)
-            week_start_index = week_start_index + 7
-            week_start_end_index = week_start_end_index + 7
+    # Day
+    row = 5
+    worksheet.cell(row=row, column=column_i).value = day_name
+    worksheet.cell(row=row, column=column_i).style = week_even if week_is_even else week_odd
 
-        worksheet.cell(row=5, column=ii + matrix_start).value = day_names[day]
-        worksheet.cell(row=5, column=ii + matrix_start).alignment = center_align
-        worksheet.cell(row=5, column=ii + matrix_start).font = small
-        worksheet.cell(row=6, column=ii + matrix_start).value = day_number
-        worksheet.cell(row=6, column=ii + matrix_start).alignment = center_align
-        worksheet.cell(row=6, column=ii + matrix_start).font = small
+    # Day of month
+    row = 6
+    month_day_style = styles['month_day_' + str(month)]
+    worksheet.cell(row=row, column=column_i).value = day_number
+    worksheet.cell(row=row, column=column_i).style = month_day_style
 
-        # print(i, year, month, month_names[month - 1], day_number, week_number, day, day_names[day])
+    # matrix row styling
+    for row_i in range(matrix_v_start, matrix_v_end + 1):
+        worksheet.cell(row=row_i, column=column_i).style = matrix_cell_even if week_is_even else matrix_cell_odd
 
 print(path)
 wb.save(filename=path)
---
+++ time_iterate.py
@@ -0,0 +1,117 @@
+import calendar
+import datetime
+from enum import Enum
+from math import floor
+
+
+class TimeIteratorMode(Enum):
+    MONTHS = 1
+    WEEKS = 2
+    DAYS = 7
+    DAYS_NO_WEEKEND = 5
+
+
+class TimeIterator:
+    day_names = ('ma', 'di', 'wo', 'do', 'vr', 'za', 'zo')
+    month_names = ('Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December')
+    calendar = calendar.Calendar()
+    __modes = ('months', 'weeks', 'days', 'days_without_weekend')
+
+    def __init__(self, start_year, start_month, months_duration, mode: TimeIteratorMode = TimeIteratorMode.DAYS):
+        self.start_year = start_year
+        self.start_month = start_month
+        self.months_duration = months_duration
+        self.mode = mode
+        self.end_year = start_year + floor((months_duration - start_month) / 12) + 1
+        self.end_month = (months_duration - (12 - start_month)) % 12 - 1
+
+    @staticmethod
+    def get_month_name(month):
+        return TimeIterator.month_names[month - 1]
+
+    @staticmethod
+    def get_day_name(day):
+        return TimeIterator.day_names[day]
+
+    def days_in_month(self, year, month):
+        return len([x for x in self.calendar.itermonthdays2(year, month) if x[0] !=0 and x[1] < 5]) if self.mode == TimeIteratorMode.DAYS_NO_WEEKEND else calendar.monthrange(year, month)[1]
+
+    def days_left_in_year(self, year, start_month=1, end_month=None):
+
+        days_in_year = 0
+        end = 13
+
+        if end_month is not None:
+            start_month = 1
+            end = end_month + 1
+
+        for d in range(start_month, end):
+            dm = self.days_in_month(year, d)
+            days_in_year += dm
+
+        return days_in_year
+
+    def iterate_full(self):
+        ym_start = 12 * self.start_year + self.start_month - 1
+        ym_end = 12 * self.end_year + self.end_month
+
+        passed_months = 0
+        i = 0
+        ii = 0
+
+        days_left_in_year = self.days_left_in_year(self.start_year, self.start_month)
+        previous_year = self.start_year
+        for ym in range(ym_start, ym_end):
+            y, m = divmod(ym, 12)
+            year = y
+            month = m + 1
+            month_name = self.get_month_name(month)
+            passed_months += 1
+            days_in_month = self.days_in_month(year, month)
+
+            if previous_year < year:
+                days_left_in_year = self.days_left_in_year(year, end_month=self.end_month) if self.end_year == year else self.days_left_in_year(year)
+                previous_year = year
+
+            if self.mode == TimeIteratorMode.MONTHS:
+                yield passed_months, year, days_left_in_year, month, month_name, days_in_month
+                continue
+
+            for day_number in self.calendar.itermonthdays(year, month):
+                if day_number == 0:
+                    continue
+                ii += 1
+
+                week_number = datetime.date(year, month, day_number).isocalendar().week
+
+                if self.mode == TimeIteratorMode.WEEKS:
+                    _last_week_of_year = datetime.date(year, 12, 31).isocalendar().week
+
+                    if ii % 7 == 1:
+                        i += 1
+                        yield passed_months, year, days_left_in_year, month, month_name, days_in_month, i, week_number
+
+                    continue
+
+                if self.mode in (TimeIteratorMode.DAYS, TimeIteratorMode.DAYS_NO_WEEKEND):
+                    day = calendar.weekday(year, month, day_number)
+
+                    if self.mode == TimeIteratorMode.DAYS_NO_WEEKEND and day in (5, 6):
+                        continue
+
+                    i += 1
+                    day_name = self.get_day_name(day)
+
+                    yield passed_months, year, days_left_in_year, month, month_name, days_in_month, i, week_number, day_number, day, day_name
+
+    def __len__(self):
+        return sum(1 for _ in self.iterate_full())
+
+    def __iter__(self):
+        return self.iterate_full()
+
+    def __str__(self):
+        s = f"TimeIterator for {self.start_year}-{self.start_month} until {self.end_year}-{self.end_month} taking {self.months_duration} months in {self.mode.name} mode"
+        return s
+
+
